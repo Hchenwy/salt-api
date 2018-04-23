aa7609077b11738092db247828e7b827d6c8bd33
chenwy <chenwy@aotain.com>
2018-04-23 19:20:00 +0800

    update salt_api.py

--- src/salt_api.py
+++ src/salt_api.py
@@ -16,10 +16,12 @@ class SaltAPI(object):
         self.__password = password
         self.__HEADER['X-Auth-Token'] = self.__salt_login()
 
-    ''' @function: 发起https请求
-        @param: url 请求url地址、 data 为空则为GET请求方式，反之为POST请求方式
-        @return:返回dict格式结果 '''
     def __deal_request(self, url, data=''):
+        ''' 发起https请求
+        :param url: 请求的url地址
+        :param data: 请求数据
+        :return: （dict）响应数据内容
+        '''
         request = urllib.request.Request(url=url, headers=self.__HEADER, data=data) if data else urllib.request.Request(url=url, headers=self.__HEADER)
         response = urllib.request.urlopen(request)
         try:
@@ -28,10 +30,10 @@ class SaltAPI(object):
         except Exception:
             raise Exception
 
-    ''' @function:登陆获取token 
-        @param: 无需传参
-        @return: 返回string格式秘钥token '''
     def __salt_login(self):
+        ''' 登录获取token
+        :return: （str）token字符串
+        '''
         params = {'eauth': 'pam', 'username': self.__username, 'password': self.__password}
         encode = urllib.parse.urlencode(params)
         data = urllib.parse.unquote(encode).encode('utf-8')
@@ -42,10 +44,12 @@ class SaltAPI(object):
         except Exception:
             raise Exception
 
-    ''' @function:远程执行命令 
-        @param: 字典形式参数传入
-        @return: 返回dict格式的执行结果 '''
     def run_cmd(self, **kwargs):
+        '''
+        远程执行命令
+        :param kwargs: 字典形式的参数传入
+        :return: （dict）主机对应的执行结果
+        '''
         if not kwargs:
             print('Error: params input error.')
             return {}
@@ -57,10 +61,11 @@ class SaltAPI(object):
         except Exception:
             raise Exception
 
-    ''' @function:获取minions主机列表 
-        @param: 无需参数
-        @return: 返回list格式主机信息 '''
     def get_minions(self):
+        '''
+        获取minions主机列表
+        :return: (list)minions信息
+        '''
         params = {'client': 'wheel', 'fun': 'key.list_all'}
         encode = urllib.parse.urlencode(params)
         data = urllib.parse.unquote(encode).encode('utf-8')
